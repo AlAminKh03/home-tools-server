5b96c415b53180800d9570d255e3850537bec685
AlAminKh03 <alaminkhan6203@gmail.com>
2022-05-26 00:47:11 +0600

    collection implemented

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwvvi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('home-tools').collection('products')
+
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Hello from home tools')
+})
+
+app.listen(port, () => {
+    console.log(`Home tool listening on port ${port}`)
+})
