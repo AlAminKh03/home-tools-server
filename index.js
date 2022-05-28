15ad82e4394390f8945b45199e7d26ff941c77f5
AlAminKh03 <alaminkhan6203@gmail.com>
2022-05-28 15:44:45 +0600

    sevral thing added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -16,12 +16,47 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('home-tools').collection('products')
-
+        const orderCollection = client.db('home-tools').collection('orders')
+        const reviewCollection = client.db('home-tools').collection('reviews')
+        // Products/
         app.get('/products', async (req, res) => {
             const query = {}
             const cursor = productCollection.find(query)
             const products = await cursor.toArray()
             res.send(products)
+        })
+
+        // reviews
+        app.get('/reviews', async (req, res) => {
+            const query = {}
+            const cursor = reviewCollection.find(query)
+            const reviews = await cursor.toArray()
+            res.send(reviews)
+        })
+
+        // single product
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.findOne(query)
+            res.send(result)
+        })
+
+        // product order 
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const result = await orderCollection.insertOne(orders)
+            res.send(result)
+        })
+
+        // get product order 
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const order = await orderCollection.find(query).toArray()
+            res.send(order)
         })
     }
     finally {
