a285cffe200724f473b9a2d4b9c8775a29a0ec33
AlAminKh03 <alaminkhan6203@gmail.com>
2022-05-28 23:10:51 +0600

    added

--- index.js
+++ index.js
@@ -111,6 +111,30 @@ async function run() {
             res.send({ result, token });
         });
 
+
+        app.get('/user', async (req, res) => {
+            const query = {}
+            const result = await userCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.put('/user/makeadmin/:email', async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const updateDoc = {
+                $set: { role: 'admin' }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        app.get('/user/admin/:email', async (req, res) => {
+            const userEmail = req.params.email
+            const user = await userCollection.findOne({ email: userEmail })
+            const isAdmin = user.role === 'admin'
+            res.send({ admin: isAdmin })
+        })
+
         // profile update 
     }
     finally {
