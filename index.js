4e123a1ec92498d6bde585f8858d3f4f2219b8f5
unknown <alaminkhan6203@gmail.com>
2023-03-23 15:56:23 +0600

    done again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwvvi.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://home-tools-v2:F1Uxzqe6JhERvhUA@cluster0.vwvvi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
